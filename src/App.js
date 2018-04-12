d63df49e28eaf59b0779ccf9710e72da7505541a
Leif Swenson <leifswenson@gmail.com>
2018-04-12 21:01:32 +0000

    12 cards render in section and pseudo code some logic in app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import Section from "./components/Section";
 import PlayerCard from "./components/PlayerCard";
 import Footer from "./components/Footer";
 import players from "./players.json";
 import "./App.css";
 
 class App extends Component {
+  
+// Score starts at 0
+// high score will start at 0
+// empty array will hold gusses
+// User will click on a player
+// if clicked player is an index of the array score will reset array will empty and score will be checked with total score
+// if clicked player is not an index of array, add to score 
+// 
 
-render() {
+
+
+  render() {
     return (
       <div>
         <Navbar />
         <Jumbotron />
-        <PlayerCard
-          name={players[0].name}
-          image={players[0].image}
-        />
+          <Section>
+          {players.map(player => (
+            <PlayerCard
+              id={player.id}
+              key={player.id}
+              name={player.name}
+              image={player.image}
+            />
+          ))}
+          </Section>
         <Footer />
       </div>
     );
   }
 }
+
+
+
 
 export default App;
 
