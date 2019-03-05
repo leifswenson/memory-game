66f232271c7faa81e44b50ea85d002ca7a596507
Leif Swenson <leifswenson@gmail.com>
2019-03-05 15:08:40 -0600

    added photos to images folder

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,79 +6,61 @@ import PlayerCard from "../PlayerCard";
 import Footer from "../Footer";
 import players from "../../players.json";
 
-class App extends Component {
-  
-  // Score starts at 0
-  // high score will start at 0
-  // empty array will hold gusses
+class Game extends Component {
   state = {
+    players,
     score: 0,
     topScore: 0,
     guessed: []
   };
 
-  
   // function to handle when a user clicks on a player
-  handleClick = (id) => {
-    
+  handleClick = id => {
     // if clicked player is not an index of array
     if (this.state.guessed.indexOf(id) === -1) {
-      
       if (this.state.score === 12) {
-        this.setState({score: 0});
-        this.setState({guessed: []});
+        this.setState({ score: 0 });
+        this.setState({ guessed: [] });
       } else {
         //push into guessed array
         this.state.guessed.push(id);
         // increment score
         this.setState({ score: this.state.score + 1 });
       }
-      
+
       // shuffle cards ??
     } else {
       // if clicked player is an index of the array score will reset array will empty and score will be checked with total score
       // show top score
-      this.setState({topScore: this.state.score});
+      this.setState({ topScore: this.state.score });
       // reset score
-      this.setState({score: 0});
-      this.setState({guessed: []});
+      this.setState({ score: 0 });
+      this.setState({ guessed: [] });
       // shuffle cards ??
     }
     console.log(this.state.guessed);
-
-    
   };
-
-
-
-
 
   render() {
     return (
       <div>
-        <Navbar 
-          score={this.state.score}
-          topScore={this.state.topScore}
-        />
+        <Navbar score={this.state.score} topScore={this.state.topScore} />
         <Jumbotron />
-          <Section>
+        <Section>
           {players.map(player => (
             <PlayerCard
+              key={player.id}
               id={player.id}
               name={player.name}
               image={player.image}
               handleClick={this.handleClick}
             />
           ))}
-          </Section>
+        </Section>
         <Footer />
       </div>
     );
   }
 }
 
-
-
-
-export default App;
-
+export default Game;
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Jumbotron.css";
 
-
 const Jumbotron = () => (
   <div className="jumbotron jumbotron-fluid">
     <div className="container">
       <h1 className="display-4 text-center">Sports Memory</h1>
-      <p className="lead text-center">Click on an image to earn points, but don't click on any more than once!</p>
+      <p className="lead text-center">
+        Click on an image to earn points, but don't click on any images more
+        than once!
+      </p>
     </div>
   </div>
 );
